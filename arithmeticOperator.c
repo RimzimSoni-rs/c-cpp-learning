e962966f9517f7bb9728039aea883355a50ad089
RimzimSoni-rs <rimzimsoni1008@gmail.com>
2026-02-26 17:42:06 +0530

    Added a Arithmetic program

---
+++ arithmeticOperator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int a,b;
+
+    printf("Enter value of a and b:");
+
+    scanf("%d%d", &a,&b);
+
+    //arithmetic operator
+    printf("a + b =%d \n", a+b);
+    printf("a - b =%d \n", a-b);
+    printf("a * b =%d \n", a*b);
+    printf("a mod b =%d \n", a%b);
+    printf("a / b =%d \n", a/b);
+    
+}
